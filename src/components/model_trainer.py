c57b8e3c58a9b565b244b6197091eec74f01563b
kalyanam13 <kalyanamchakrawarty66@gmail.com>
2026-05-12 10:54:31 +0530

    Added hyperparameter tunning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -49,23 +49,55 @@ class ModelTrainer:
             }
 
 
-            model_report: dict = {
-                "Random Forest": r2_score(y_test, models["Random Forest"].fit(X_train, y_train).predict(X_test)),
-                "Decision Tree": r2_score(y_test, models["Decision Tree"].fit(X_train, y_train).predict(X_test)),
-                "Gradient Boosting": r2_score(y_test, models["Gradient Boosting"].fit(X_train, y_train).predict(X_test)),
-                "Linear Regression": r2_score(y_test, models["Linear Regression"].fit(X_train, y_train).predict(X_test)),
-                "K-Neighbors Regressor": r2_score(y_test, models["K-Neighbors Regressor"].fit(X_train, y_train).predict(X_test)),
-                "XGBRegressor": r2_score(y_test, models["XGBRegressor"].fit(X_train, y_train).predict(X_test)),
-                "CatBoosting Regressor": r2_score(y_test, models["CatBoosting Regressor"].fit(X_train, y_train).predict(X_test)),
-                "AdaBoost Regressor": r2_score(y_test, models["AdaBoost Regressor"].fit(X_train, y_train).predict(X_test))
-            }
+            params={
+
+                    "Decision Tree": {
+                        'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+
+                    "Random Forest":{
+                        'n_estimators': [8,16,32,64,128,256]
+                },
+
+                    "Gradient Boosting":{
+                        'learning_rate':[.1,.01,.05,.001],
+                        'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                        'n_estimators': [8,16,32,64,128,256]
+                },
+
+                    "Linear Regression":{},
+
+                    "K-Neighbors Regressor":{
+                            'n_neighbors':[3,5,7,9],
+                            'weights':['uniform','distance'],
+                            'algorithm':['auto','ball_tree','kd_tree','brute']
+                },
+
+                    "XGBRegressor":{
+                            'learning_rate':[.1,.01,.05,.001],
+                            'n_estimators': [8,16,32,64,128,256]
+                },
+
+                    "CatBoosting Regressor":{
+                        'depth': [6,8,10],
+                        'learning_rate': [0.01, 0.05, 0.1],
+                        'iterations': [30, 50, 100]
+                },
+
+                    "AdaBoost Regressor":{
+                        'learning_rate':[.1,.01,0.5,.001],
+                        'n_estimators': [8,16,32,64,128,256]
+                }
+
+        }
 
             model_report = evaluate_models(
                                             X_train=X_train,
                                             y_train=y_train,
                                             X_test=X_test,
                                             y_test=y_test,
-                                            models=models
+                                            models=models,
+                                            params=params
             )
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
@@ -75,6 +107,8 @@ class ModelTrainer:
                 list(model_report.values()).index(best_model_score)
             ]
             best_model = models[best_model_name]
+
+            best_model.fit(X_train, y_train)
 
             if best_model_score < 0.6:
                 raise CustomException("No best model found", sys)
